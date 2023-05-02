192c9a74acf31d024b5a89a33c2515d6255cb87b
Anthony Ranieri <anthonyjaranieri@gmail.com>
2023-05-02 11:47:24 -0400

    just started and made the file structure

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
+import ModalWithForm from './ModalWithForm/ModalWithForm';
+import ItemModal from './ItemModal/ItemModal';
+
+function App() {
+  return(
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+      <ModalWithForm />
+      <ItemModal />
+    </div>
+  )
+}
+
+export default App;
